70bf4524e5ad4d7a47cb2f9e2ac405ddcd07d5bf
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-06-17 08:24:01 +0530

    error page and footer

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Error from '../images/404_error.svg'
 
 const ErrorPage = () => {
   return (
-    <div>
-      ErrorPage
-    </div>
+    <section className='error-page'>
+      <div className="error_container">
+        <div className="error_right">
+          <img src={Error} alt='error img'></img>
+        </div>
+        <div className="error_left">
+          <h1>Oops...</h1>
+          <h2>Page not found</h2>
+          <p>We couldn't find the page you were looking for.</p>
+          <Link to='/' className='btn primary'>Go Back to Homepage</Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
